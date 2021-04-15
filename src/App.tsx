c2dd2ca2345bed91206cb5f0dad029aace795323
aca-ta <seljydef@gmail.com>
2021-04-16 01:03:33 +0900

    wip: load csv data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,6 @@
-import Leaflet from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import KosodateMap from "./Map";
 
-Leaflet.Icon.Default.imagePath =
-  "//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/";
-
-//船橋市役所のlat lon
-const position: [number, number] = [35.694722, 139.9825];
 const App = () => {
-  return (
-    <MapContainer
-      center={position}
-      zoom={13}
-      scrollWheelZoom={false}
-      style={{ height: "100vh" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
-  );
+  return <KosodateMap />;
 };
 export default App;
