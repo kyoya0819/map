4dab2cfabdde1ba42dd0d2d8233d6a3dfe1fc22a
aca-ta <seljydef@gmail.com>
2021-04-16 01:20:27 +0900

    add.

---
+++ src/Map.tsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import Leaflet from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+Leaflet.Icon.Default.imagePath =
+  "//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/";
+
+interface INurserySchool {
+  name: string;
+  address: string;
+  phoneNumber: string;
+  lat: number;
+  lon: number;
+}
+
+const loadFeatures = async () => {
+  const { data } = await axios.get(
+    "https://raw.githubusercontent.com/Code-for-Funabashi/kosodate-map/main/geodata/%E4%B8%80%E6%99%82%E4%BF%9D%E8%82%B2.csv"
+  );
+  console.log(data);
+  return data;
+};
+
+//船橋市役所のlat lon
+const position: [number, number] = [35.694722, 139.9825];
+const kosodateMap = () => {
+  loadFeatures();
+  return (
+    <MapContainer
+      center={position}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ height: "100vh" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default kosodateMap;
