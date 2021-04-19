3a38500024de8152bce8db62c897b4944ef3cfed
aca-ta <seljydef@gmail.com>
2021-04-20 01:16:00 +0900

    wip: map a feature.

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,36 +1,55 @@
-import axios from "axios";
+import { useState, useEffect } from "react";
+import axios, { AxiosResponse } from "axios";
 import Leaflet from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import FeaturePoint from "./FeaturePoint";
 Leaflet.Icon.Default.imagePath =
   "//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/";
 
-interface IPointDetail {
+interface IFeatureList {
+  url: string;
+  type: string;
+}
+
+interface IFeaturePointDetail {
   address?: string;
   phone_number?: string;
 }
 
-interface INurserySchool {
+export interface IFeaturePoint {
   name: string;
   address: string;
   phoneNumber: string;
   lat: number;
-  lon: number;
-  details: IPointDetail;
+  lng: number;
+  details: IFeaturePointDetail;
 }
 
-const loadFeatures = async () => {
-  const { data } = await axios.get<[INurserySchool]>(
-    "https://raw.githubusercontent.com/Code-for-Funabashi/Scrape-OpenData/kosodate-map/geodata/projects/kosodate-map/%E4%B8%80%E6%99%82%E4%BF%9D%E8%82%B2.json"
+const featureList: [IFeatureList] = [
+  {
+    url:
+      "https://raw.githubusercontent.com/Code-for-Funabashi/Scrape-OpenData/kosodate-map/geodata/projects/kosodate-map/%E4%B8%80%E6%99%82%E4%BF%9D%E8%82%B2.json",
+    type: "保育園",
+  },
+];
+
+const loadFeatures = async (url: string) => {
+  const res: AxiosResponse<[IFeaturePoint]> = await axios.get<[IFeaturePoint]>(
+    url
   );
-  console.log(data);
-  return data;
+  console.log(res.data);
+  return res.data;
 };
 
 //船橋市役所のlat lon
 const position: [number, number] = [35.694722, 139.9825];
-const kosodateMap = () => {
-  loadFeatures();
+
+const KosodateMap = () => {
+  const [features, setFeatures] = useState<IFeaturePoint[]>([]);
+  useEffect(() => {
+    loadFeatures(featureList[0].url).then((data) => setFeatures(data));
+  });
   return (
     <MapContainer
       center={position}
@@ -42,13 +61,9 @@ const kosodateMap = () => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <FeaturePoint point={features} />
     </MapContainer>
   );
 };
 
-export default kosodateMap;
+export default KosodateMap;
